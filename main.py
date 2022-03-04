b916dd779e7c100656780a15c735b007e68f9ca9
RYLiang18 <richard.y.liang@gmail.com>
2022-03-03 21:29:40 -0800

    feat: bot complete, added note about bot detection

--- main.py
+++ main.py
@@ -1,14 +1,45 @@
-import requests, json
+import requests, json, os, time, random, pprint
+
+pp = pprint.PrettyPrinter(indent = 2)
 
 """
 Note on Bot Detection
-
-https://www.reddit.com/r/Genshin_Impact/comments/rohk7w/quick_tutorial_for_building_your_own_hoyolab/
 
 When writing bots, it is recommended that your requests resemble your browser as much as possible
 This is to keep MiHoYo from figuring out that a bot is running and potentially banning your account 
 """
 
+# pretend that we are a browser hehe
 user_agent = "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/99.0.4844.51 Safari/537.36"
 origin = "https://webstatic-sea.mihoyo.com"
 referer = "https://webstatic-sea.mihoyo.com/"
+
+act_id = os.environ.get("act_id")
+mhy_uuid=os.eviron.get("mhy_uuid")
+
+url = "https://hk4e-api-os.mihoyo.com/event/sol/sign?lang=en-us"
+
+payload = f"{{\"act_id\": \"{act_id}\"}}"
+
+header = {
+    "Cookie": f"_MHYUUID={mhy_uuid}",
+    "User-Agent": user_agent,
+    "Origin": origin,
+    "Referer": referer
+}
+
+
+# wait random time of 1 - 3600 seconds
+
+print("============= bot start =============")
+
+res = requests.post(
+    url,
+    data = payload,
+    headers = header
+)
+
+res_json = res.json()
+
+print("Bot Results...")
+pp.pprint(res_json)
